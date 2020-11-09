09d8f1fe8c931eba30d7b908ad257452feff6500
devdrums <devdrums@macbook-pro.myfiosgateway.com>
2020-11-09 13:43:40 -0500

    feat: test route

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const router = require("./api/router");
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "../dist/static")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api", router);
+
+module.exports = app;
