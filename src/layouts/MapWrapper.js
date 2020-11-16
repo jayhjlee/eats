b65dc76e492272e63612f23e2d899257f2bb498f
devdrums <devdrums@macbook-pro.myfiosgateway.com>
2020-11-16 13:04:55 -0500

    feat: rendering map

--- src/layouts/MapWrapper.js
+++ src/layouts/MapWrapper.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import mapboxgl from "mapbox-gl";
+import secrets from "../../secrets";
+
+mapboxgl.accessToken = secrets.mapBoxKey;
 
 class MapWrapper extends Component {
 	constructor(props) {
 		super(props);
+		this.map = React.createRef();
 	}
+
+	componentDidMount() {
+		this.map = new mapboxgl.Map({
+			container: "MapContainer",
+			style: "mapbox://styles/mapbox/streets-v11",
+			center: [-74.0567, 40.7992],
+			zoom: 15,
+		});
+	}
+
 	render() {
 		const { isLoggedIn, token, user } = this.props;
 		if (!isLoggedIn && !token && !user) return <Redirect to="/log-in" />;
 
-		return <div></div>;
+		return (
+			<section>
+				<div id="MapContainer" className="mapContainer" />
+			</section>
+		);
 	}
 }
 
